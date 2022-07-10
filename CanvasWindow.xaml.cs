734262687c0824e960cfd61a1636f7a04272a973
thepicture <85333076+thepicture@users.noreply.github.com>
2022-07-10 15:34:27 +0500

    feat(Canvas): add undo and redo

--- CanvasWindow.xaml.cs
+++ CanvasWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Ink;
 using System.Windows.Media;
 
 namespace CanvasDrawingDesktopApp
@@ -10,9 +14,21 @@ namespace CanvasDrawingDesktopApp
     /// </summary>
     public partial class CanvasWindow : Window
     {
+        private List<Stroke> _strokes = new List<Stroke>();
+        private bool _isChanging;
+
         public CanvasWindow()
         {
             InitializeComponent();
+            Canvas.Strokes.StrokesChanged += HandleStrokesChanged;
+        }
+
+        private void HandleStrokesChanged(object sender, StrokeCollectionChangedEventArgs e)
+        {
+            if (_isChanging)
+                return;
+            if (e.Added.Count > 0)
+                _strokes = Canvas.Strokes.ToList();
         }
 
         private void HandleColorChanged(object sender, SelectionChangedEventArgs e)
@@ -50,8 +66,28 @@ namespace CanvasDrawingDesktopApp
             }
             else
             {
-                throw new System.Exception("Drawing type is not implemented");
+                throw new Exception("Drawing type is not implemented");
             }
+        }
+
+        private void HandleUndo(object sender, RoutedEventArgs e)
+        {
+            if (Canvas.Strokes.Count == 0)
+                return;
+            _isChanging = true;
+            List<Stroke> cachedStrokes = Canvas.Strokes.ToList();
+            Canvas.Strokes.Clear();
+            Canvas.Strokes.Add(new StrokeCollection(cachedStrokes.Take(cachedStrokes.Count - 1)));
+            _isChanging = false;
+        }
+
+        private void HandleRedo(object sender, RoutedEventArgs e)
+        {
+            _isChanging = true;
+            int lastCount = Canvas.Strokes.Count;
+            Canvas.Strokes.Clear();
+            Canvas.Strokes.Add(new StrokeCollection(_strokes.Take(lastCount + 1)));
+            _isChanging = false;
         }
     }
 }
